8e288d65335f33a3b2d7c63959ef72fbbd056538
dplocki <dawid.plocki@gmail.com>
2025-02-01 15:55:47 +0100

    feat: respond with correctionId

--- src/main.cs
+++ src/main.cs
@@ -6,4 +6,19 @@ Console.WriteLine("Logs from your program will appear here!");
 
 var server = new TcpListener(IPAddress.Any, 9092);
 server.Start();
-server.AcceptSocket();
+
+int correlationId = 7;
+
+using var client = await server.AcceptTcpClientAsync(); // Accept client connection
+Console.WriteLine("Client connected!");
+
+var stream = client.GetStream();
+
+var messageSizeBytes = BitConverter.GetBytes(0);
+Array.Reverse(messageSizeBytes);
+await stream.WriteAsync(messageSizeBytes);
+
+var messageContentBytes = BitConverter.GetBytes(correlationId);
+Array.Reverse(messageContentBytes);
+await stream.WriteAsync(messageContentBytes);
+await stream.FlushAsync();
