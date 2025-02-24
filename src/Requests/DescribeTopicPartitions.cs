78c2d1ea5fb67b77d137ea45022b9bf033707161
dplocki <dawid.plocki@gmail.com>
2025-02-24 21:27:14 +0100

    feat: enhance DescribeTopicPartitions response with topic information

--- src/Requests/DescribeTopicPartitions.cs
+++ src/Requests/DescribeTopicPartitions.cs
@@ -1,12 +1,18 @@
+using System.Buffers.Binary;
+
 internal class DescribeTopicPartitions : IModule
 {
     const int UNKNOWN_TOPIC_OR_PARTITION = 3;
 
     public byte[] Respond(RequestMessage requestMessage)
     {
+        var topicLength = BinaryPrimitives.ReadInt32BigEndian(requestMessage.RawRequestBody);
+
         var result = new ServerResponseDescribeTopicPartitionsMessage
         {
-            Error = UNKNOWN_TOPIC_OR_PARTITION
+            CorrelationId = requestMessage.CorrelationId,
+            Error = UNKNOWN_TOPIC_OR_PARTITION,
+            Topic = requestMessage.RawRequestBody.AsSpan(1, topicLength - 1).ToString(),
         };
 
         return result.ToMessage();
@@ -21,11 +27,15 @@ struct ServerResponseDescribeTopicPartitionsMessage
 
     public APIVersionItem[] Items;
 
+    public string Topic;
+
     public readonly byte[] ToMessage()
     {
         var builder = new ResponseBuilder();
         builder.Add32Bits(CorrelationId);
         builder.Add16Bits(Error);
+        builder.Add32Bits(1);
+        builder.Add32Bits(0);
 
         return builder.ToByteArray();
     }
